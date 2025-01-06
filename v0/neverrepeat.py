56c51f731a790b2335d8c7c2de1b7d9ad34fd35c
Luuppi <se.topsu@gmail.com>
2025-01-06 21:10:32 +0200

    added never repeat random concept

---
+++ v0/neverrepeat.py
@@ -0,0 +1,10 @@
+import random
+amount_of_pairs = 200
+
+number_list = list(range(1, amount_of_pairs + 1))
+random.shuffle(number_list)
+
+while len(number_list):
+    print(number_list)
+    number_list.pop(0)
+
