26470365b529b6f2698fad8297352fa9b5b5a83a
Luuppi <se.topsu@gmail.com>
2025-02-07 09:42:11 +0200

    finalized new gamemodes

--- v1/gameplay_modules.py
+++ v1/gameplay_modules.py
@@ -5,7 +5,8 @@ import os
 import core_functions as core
 import random
 import string
-import readline # for better input field
+import time
+import readline  # for better input field
 
 
 class MainGameplay:
@@ -18,16 +19,62 @@ class MainGameplay:
             self.question = "answer"
             self.answer = "question"
 
-        # chooses the order
-        self.order = order
-        if order == "reverse":
-            self.order = "reverse"
-
         self.pairs = pairs
         self.current_question = current_question
         self.show_question_number = core.settings_value_manipulator("show current question number")
         self.streak_current = streak_current
         self.streak_all_time = core.settings_value_manipulator("all time streak")
+        self.max_question = core.settings_value_manipulator("max question")
+        self.all_settings = core.get_options()
+        self.user_input = ""
+        self.random_list = core.never_repeat_random_list(self.all_settings["min question"], self.all_settings["max question"])
+        self.list_index = 0
+
+        # chooses the order
+        self.order = order
+        if order == "reverse":
+            self.order = "reverse"
+        elif order == "random":
+            self.order = "random"
+            self.current_question = self.random_list[0]
+
+        self.enabled_prints = {
+            "correct answer" : True,
+            "invalid seek" : True,
+            "valid seek" : True,
+            "show correct answer" : True,
+            "return to main menu" : True,
+            "empty field" : True,
+            "wrong answer" : True
+        }
+
+    def print_correct_answer(self):
+        if self.enabled_prints["correct answer"]:
+            print("Correct")
+
+    def print_invalid_seek(self, question_number):
+        if self.enabled_prints["invalid seek"]:
+            print(f'"{question_number}" is an invalid seek number.')
+
+    def print_valid_seek(self, question_number):
+        if self.enabled_prints["valid seek"]:
+            print(f"Seeking from {self.current_question} to {question_number}...")
+
+    def print_show_correct_answer(self, correct_answers):
+        if self.enabled_prints["show correct answer"]:
+            print(f"Correct answers: {" / ".join(correct_answers)}")
+
+    def print_return_to_main_menu(self):
+        if self.enabled_prints["return to main menu"]:
+            print("Returning to main menu...")
+
+    def print_empty_field(self):
+        if self.enabled_prints["empty field"]:
+            print("Please type something...")
+
+    def print_wrong_answer(self):
+        if self.enabled_prints["wrong answer"]:
+            print(f'"{self.user_input.capitalize()}" is not a valid answer.')
 
     def update_class_variables(self, variable, new_input):
         match variable:
@@ -42,34 +89,45 @@ class MainGameplay:
         print(f"Current streak: {self.streak_current}, All time streak: {self.streak_all_time}")
         print(f"{f"{self.current_question}. " if self.show_question_number else ""}{" / ".join(self.pairs[self.current_question][self.question])}")
 
-    def answer_check(self, user_input):
+    def answer_check(self):
         correct_answers = self.pairs[self.current_question][self.answer]
+        user_input = self.user_input
 
         # check for correct answer in user_input, iterates through all answer candidates
         for answer in correct_answers:
             answer = answer.lower()
             if answer == user_input or answer in user_input:
-                print("Correct")
+                self.next_question()
+                self.streak_current += 1
+                self.print_correct_answer()
                 return "correct"
 
         match = re.search(r"^seek (\d+)", user_input)
         if match:  # seeking
             question_number = int(match.group(1))
-            self.update_class_variables("current_question", question_number)
+            if question_number >= self.max_question:
+                self.print_invalid_seek(question_number)
+            else:
+                self.print_valid_seek(question_number)
+                self.update_class_variables("current_question", question_number)
+
             return "seek",
 
         elif user_input in ["c", "correct"]:  # show correct answer
-            print(f"Correct answers: {" / ".join(correct_answers)}")
+            self.update_class_variables("streak_current", 0)  # breaks the streak
+            self.print_show_correct_answer(correct_answers)
             return "show correct answer"
 
         elif user_input in ["q", "quit"]:  # quit to main menu
-            print("Returning to main menu...")
+            self.print_return_to_main_menu()
             return "quit"
 
         elif not user_input or re.findall("^ *$", user_input):  # checks if user_input is empty
-            print("Please type something...")
+            self.print_empty_field()
             return "empty field"
 
+        self.update_class_variables("streak_current", 0)  # breaks the streak
+        self.print_wrong_answer()
         return "wrong"  # if there's no match, the answer is incorrect
 
     def next_question_plus_one(self):
@@ -78,17 +136,29 @@ class MainGameplay:
     def next_question_minus_one(self):
         self.current_question -= 1
 
+    def next_question_random(self):
+        self.list_index += 1
+        try:
+            self.current_question = self.random_list[self.list_index]
+        except IndexError:  # Wrap around
+            self.list_index = 0
+            self.current_question = self.random_list[self.list_index]
+
     def next_question(self):
         # Check how the next question should be done and return it.
         if self.order == "forward":
             self.next_question_plus_one()
         elif self.order == "reverse":
             self.next_question_minus_one()
+        elif self.order == "random":
+            self.next_question_random()
         else:
             raise Exception("Someone has set an invalid question order.")
 
-    def generate_multiple_choice_answers(self, current_question, generate_new=True, correct_letter="", multiple_choice_options={}):
+    def generate_multiple_choice_answers(self, generate_new=True, correct_letter="", multiple_choice_options=None):
 
+        if multiple_choice_options is None:
+            multiple_choice_options = {}
         if generate_new:
             options = core.get_options()
             max_options = options["multiple choice max options"]
@@ -98,11 +168,11 @@ class MainGameplay:
                 raise Exception(
                     f"Not enough unique pairs to generate {max_options} options from {len(random_list)} pairs.")
 
-            if current_question in random_list:
-                random_list.remove(current_question)
+            if self.current_question in random_list:
+                random_list.remove(self.current_question)
 
             random_list = random_list[:max_options]  # one less than max_options
-            random_list.append(current_question)
+            random_list.append(self.current_question)
 
             while len(random_list) != max_options:
                 random_list.pop(0)
@@ -112,7 +182,7 @@ class MainGameplay:
             letters = string.ascii_lowercase
 
             for _, (letter, answer_number) in enumerate(zip(letters, random_list)):
-                if answer_number == current_question:
+                if answer_number == self.current_question:
                     correct_letter = letter
 
                 multiple_choice_options[letter] = self.pairs[answer_number]["answer"][0]
@@ -125,46 +195,139 @@ class MainGameplay:
             print(f"{key}. {option}")
         print()
 
-        return correct_letter, current_question, multiple_choice_options
+        return correct_letter, multiple_choice_options
 
-    @staticmethod
-    def user_input_sanitized():
-        user_input = input("Answer: ")
-        return user_input.lower()
+    def user_input_sanitized(self):
+        self.user_input = input("Answer: ").lower()
 
     @staticmethod
     def clear():
         os.system("clear")
 
+    def check_max_streak(self):
+        if self.streak_current > self.streak_all_time:
+            core.settings_value_manipulator("all time streak", "dump", self.streak_current)
+            self.streak_all_time = core.settings_value_manipulator("all time streak")
+
+    def streak_question_and_input(self):
+        self.check_max_streak()
+        self.print_question()
+        self.user_input_sanitized()
+
     def play(self):
         playing = True
-        user_input = ""
         answer_check = ""
 
         while playing:
-            self.print_question()
-            user_input = self.user_input_sanitized()
-            answer_check = self.answer_check(user_input)
+            self.streak_question_and_input()
             self.clear()  # To get the screen clear for the next question print like done in the last functions
+            answer_check = self.answer_check()
+            # Other return values have already done their job in the answer_check()
+            if answer_check == "quit":
+                return self.streak_current
+
+
+class Normal(MainGameplay):
+    pass
+
+class Reverse(MainGameplay):
+    def __init__(self, pairs, reverse=True, current_question=1, streak_current=0, order="forward"):
+        super().__init__(pairs, reverse, current_question, streak_current, order)
+
+class MultipleChoice(MainGameplay):
+    def play(self):
+        playing = True
+        answer_check = ""
+        # These are multiple choice specific
+        multiple_choice_options = {}
+        correct_letter = ""
+
+        while playing:
+            self.check_max_streak()
+            self.print_question()
+
+            # Multiple choice specific
+            if not multiple_choice_options:
+                correct_letter, multiple_choice_options = self.generate_multiple_choice_answers()
+
+            else:
+                correct_letter, multiple_choice_options = self.generate_multiple_choice_answers(generate_new=False, correct_letter=correct_letter, multiple_choice_options=multiple_choice_options)
+
+            self.user_input_sanitized()
+
+            # Multiple choice specific
+            if self.user_input == correct_letter or correct_letter in len(self.user_input.strip()) < 2:
+                self.user_input = self.pairs[self.current_question][self.answer][0]
+                self.user_input = self.user_input.lower()
+
+            self.clear()
+
+            answer_check = self.answer_check()
+            # Other return values have already done their job in the answer_check()
+            if answer_check == "quit":
+                return self.streak_current
+            elif answer_check == "correct":  # Multiple choice specific
+                multiple_choice_options = {}  # This is to make it generate new options
+
+class Survive(MainGameplay):
+    def __init__(self, pairs, reverse=False, current_question=1, streak_current=0, order="random"):
+        super().__init__(pairs, reverse, current_question, streak_current, order)
+        self.streak_all_time = core.settings_value_manipulator("all time survival streak")
+        self.lives = core.settings_value_manipulator("lives")
+        self.enabled_prints["wrong answer"] = False
+        self.enabled_prints["show correct answer"] = False
+
+
+    def check_max_streak(self):
+        if self.streak_current > self.streak_all_time:
+            core.settings_value_manipulator("all time survival streak", "dump", self.streak_current)
+            self.streak_all_time = core.settings_value_manipulator("all time survival streak")
+
+    def print_question(self):
+        print(f"Current survival streak: {self.streak_current}, All time survival streak: {self.streak_all_time}")
+        print(f"{f"{self.current_question}. " if self.show_question_number else ""}{" / ".join(self.pairs[self.current_question][self.question])}")
+
+    def return_correct_answers(self):
+        return " / ".join(self.pairs[self.current_question][self.answer])
+
+    def print_lives(self):
+        print("Lives left: ", end="")
+        for _ in range(self.lives):
+            print(" ", end="")
+        else:
+            print()
+
+    def play(self):
+        playing = True
+        answer_check = ""
+
+        while playing:
+            self.print_lives()
+            self.streak_question_and_input()
+            self.clear()  # To get the screen clear for the next question print like done in the last functions
+            answer_check = self.answer_check()
+            # Other return values have already done their job in the answer_check()
             match answer_check:
-                case "correct":
-                    self.update_class_variables("current_question", self.next_question())
-                case "seek":  # updating question variable has already been done in the function, this is easier than having to pass the question number to the main loop
-                    pass
-                case "show correct answer":
-                    self.update_class_variables("streak_current", 0)  # breaks the streak
                 case "quit":
-                    playing = False  # this quits the main loop
-                case "empty field":
-                    pass
+                    return self.streak_current
                 case "wrong":
-                    self.update_class_variables("streak_current", 0)  # breaks the streak
-                case _:
-                    raise Exception("Answer check returned an invalid result. Did you write the possible results correctly?")
+                    print(f"Wrong, correct answer: {self.return_correct_answers()}")
+                    self.current_question += 1
+                    self.lives -= 1
+                case "show correct answer":
+                    print(f"The correct answer was: {self.return_correct_answers()}")
+                    self.current_question += 1
+                    self.lives -= 1
+
+            if self.lives <= 0:
+                self.clear()
+                print(f"The correct answer was: {self.return_correct_answers()}")
+                print("Game over!")
+                time.sleep(5)
+                return 0
 
 
 def normal(pairs, current_question=1, current_question_number_enabled=True, streak_current=0, streak_all_time=0):
-
     result = True
     while result:
 
@@ -191,7 +354,6 @@ def normal(pairs, current_question=1, current_question_number_enabled=True, stre
             pass
         case _:
             raise Exception("Uhh... How...")
-    
 
 # This is almost a copypaste of normal mode, just switched places with questions and answers
 def reverse(pairs, current_question=1, current_question_number_enabled=True, streak_current=0, streak_all_time=0):
@@ -266,8 +428,6 @@ def multiple_choice(pairs, current_question=1, current_question_number_enabled=T
         case _:
             raise Exception("Uhh... How...")
 
-
-# This hasn't yet been customized for survive, it's just normal gamemode right now.
 def survive(pairs, current_question=1, current_question_number_enabled=True, streak_current=0, streak_all_time=0, lives=1):
 
     result = True
@@ -299,7 +459,6 @@ def survive(pairs, current_question=1, current_question_number_enabled=True, str
             return True, current_question, streak_current, False, lives
         case _:
             raise Exception("Uhh... How...")
-    
 
 def answer_check(correct_answers, user_input, current_question):
     for answer in correct_answers:
@@ -331,11 +490,9 @@ def answer_check(correct_answers, user_input, current_question):
         print("Wrong")
         return "wrong", current_question
 
-
 def next_question_plus_one(current_question):
     current_question += 1
     return current_question
-
 
 def next_question_minus_one(current_question):
     current_question -= 1
@@ -349,7 +506,7 @@ def streak_functions(current_streak=0, mode="add"):
     else:
         raise Exception("Invalid selection")
 
-def generate_multiple_choice_answers(pairs, current_question, generate_new=True, correct_letter="", multiple_choice_options={}):
+def generate_multiple_choice_answers(pairs, current_question, generate_new=True, correct_letter="", multiple_choice_options=None):
 
     if generate_new:
         options = core.get_options()
@@ -387,7 +544,6 @@ def generate_multiple_choice_answers(pairs, current_question, generate_new=True,
     print()
     
     return correct_letter, current_question, multiple_choice_options
-
 
 def print_question(current_question, current_question_number_enabled, questions):
     print(f"{f"{current_question}. " if current_question_number_enabled else ""}{" / ".join(questions)}")
--- v1/main.py
+++ v1/main.py
@@ -1,7 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
-import os
 import argparse
-import time
 
 import core_functions as core
 import gameplay_modules as gameplay
@@ -16,7 +14,7 @@ args = parser.parse_args()
 # make pair list
 pairs = core.pair_import(args.i)
 
-game_version = "v1"
+game_version = "v1.1"
 modes = [
     ["Normal", "n"],                            #0
     ["Normal Random", "nr"],                    #1
@@ -46,8 +44,81 @@ all_time_streak = core.settings_value_manipulator("all time streak")
 lives_left = core.settings_value_manipulator("lives")
 all_time_streak_survival = core.settings_value_manipulator("all time survival streak")
 
-# setting up before gameplay loop
+# Initialize game variable
+game = gameplay.Normal(pairs)
 
+# New gameplay loop here
+while not False:  # I know this is cursed
+    # Main menu
+    selected_mode = core.main_menu(modes=modes, version=game_version)
+
+    game.clear()  # stupid way to clear
+    print("Now playing:", modes[selected_mode][0]) if selected_mode < 10 else None
+
+    match selected_mode:
+        case 0:  # Normal
+            game = gameplay.Normal(pairs, current_question=max_question, streak_current=current_streak, order="reverse")
+
+            current_streak = game.play()
+        case 1:  # Normal random
+            game = gameplay.Normal(pairs, streak_current=current_streak, order="random")
+
+            current_streak = game.play()
+        case 2:  # Normal reverse order
+            game = gameplay.Normal(pairs)
+            game.order = "reverse"
+            game.streak_current = current_streak
+
+            current_streak = game.play()
+        case 3:  # Reverse
+            game = gameplay.Reverse(pairs)
+            game.streak_current = current_streak
+
+            current_streak = game.play()
+        case 4:  # Reverse random
+            game = gameplay.Reverse(pairs, streak_current=current_streak, order="random")
+            game.streak_current = current_streak
+
+            current_streak = game.play()
+        case 5:  # Reverse reverse order
+            game = gameplay.Reverse(pairs, current_question=max_question, streak_current=current_streak, order="reverse")
+            game.streak_current = current_streak
+
+            current_streak = game.play()
+        case 6:  # Multiple choice
+            game = gameplay.MultipleChoice(pairs)
+            game.streak_current = current_streak
+
+            current_streak = game.play()
+        case 7:  # Multiple choice random
+            game = gameplay.MultipleChoice(pairs, streak_current=current_streak, order="random")
+            game.streak_current = current_streak
+
+            current_streak = game.play()
+        case 8:  # Multiple choice reverse order
+            game = gameplay.MultipleChoice(pairs, streak_current=current_streak, current_question=max_question, order="reverse")
+            game.streak_current = current_streak
+
+            current_streak = game.play()
+        case 9:  # Survive!
+            game = gameplay.Survive(pairs)
+            game.streak_current = current_streak
+
+            current_streak = game.play()
+        case 10:  # The empty option
+            raise Exception("Out of all the options, you had to choose the empty one... :/")
+        case 11:  # Settings
+            options = core.get_options(mode="load")
+            core.settings_menu(options, locked_values=locked_settings_values, static_values=static_settings_values)
+
+            # Reload needed options
+            max_question = core.settings_value_manipulator("max question")
+        case 12:  # Quit
+            exit(0)
+
+# LEGACY LOOP!
+raise Exception("You broke out of the main loop, somehow...")
+# setting up before gameplay loop
 while True:
     selected_mode = core.main_menu(modes=modes, version=game_version)
 
@@ -58,7 +129,7 @@ while True:
         case 2 | 5 | 8:
             current_question = len(pairs) - 1
             playing = True
-        case 1 | 4 | 7 | 9:
+        case 1 | 4 | 7 | 9: # randoms
             shuffled_questions = core.never_repeat_random_list(min_question, max_question)
             current_question = 0
             lives_left = core.settings_value_manipulator("lives")
@@ -90,7 +161,6 @@ while True:
 
     # gameplay loop here
     while playing:
-
         # gamemodes
         if selected_mode in [0, 1, 2, 9]: # normal + survive
             if selected_mode == 1:
