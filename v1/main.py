22dff0cf0eb24b62ed0743fbd154ab5e4f5322fc
Luuppi <se.topsu@gmail.com>
2025-02-07 13:25:31 +0200

    removed legacy code

--- v1/main.py
+++ v1/main.py
@@ -55,9 +55,11 @@ while not False:  # I know this is cursed
     game.clear()  # stupid way to clear
     print("Now playing:", modes[selected_mode][0]) if selected_mode < 10 else None
 
+    # Matches the main mode. Aka. Normal, Reverse, Multiple Choice and Survive!
+    # Then in a sub condition does the setting for the sub game mode.
     match selected_mode:
-        case 0:  # Normal
-            game = gameplay.Normal(pairs, current_question=max_question, streak_current=current_streak, order="reverse")
+        case 0:  # Normals
+            game = gameplay.Normal(pairs, current_question=max_question, streak_current=current_streak)
 
             current_streak = game.play()
         case 1:  # Normal random
@@ -65,9 +67,7 @@ while not False:  # I know this is cursed
 
             current_streak = game.play()
         case 2:  # Normal reverse order
-            game = gameplay.Normal(pairs)
-            game.order = "reverse"
-            game.streak_current = current_streak
+            game = gameplay.Normal(pairs, current_question=max_question, streak_current=current_streak, order="reverse")
 
             current_streak = game.play()
         case 3:  # Reverse
@@ -115,125 +115,3 @@ while not False:  # I know this is cursed
             max_question = core.settings_value_manipulator("max question")
         case 12:  # Quit
             exit(0)
-
-# LEGACY LOOP!
-raise Exception("You broke out of the main loop, somehow...")
-# setting up before gameplay loop
-while True:
-    selected_mode = core.main_menu(modes=modes, version=game_version)
-
-    match selected_mode:
-        case 0 | 3 | 6:
-            current_question = 1
-            playing = True
-        case 2 | 5 | 8:
-            current_question = len(pairs) - 1
-            playing = True
-        case 1 | 4 | 7 | 9: # randoms
-            shuffled_questions = core.never_repeat_random_list(min_question, max_question)
-            current_question = 0
-            lives_left = core.settings_value_manipulator("lives")
-            playing = True
-            if selected_mode == 9:
-                current_streak = 0
-        case 11:
-            options = core.get_options(mode="load")
-            core.settings_menu(options, locked_values=locked_settings_values, static_values=static_settings_values)
-
-            # Reload options
-            all_time_streak = core.settings_value_manipulator("all time streak")
-            all_time_streak_survival = core.settings_value_manipulator("all time survival streak")
-            min_question = core.settings_value_manipulator("min question")
-            max_question = core.settings_value_manipulator("max question")
-            show_current_question_number = core.settings_value_manipulator("show current question number")
-            lives_left = core.settings_value_manipulator("lives")
-            
-            playing = False
-        case 12:
-            exit(0)
-        case 10:
-            raise Exception("Out of all the options, you had to choose the empty one... :/")
-
-    all_time_streak = core.settings_value_manipulator("all time streak")
-    all_time_streak_survival = core.settings_value_manipulator("all time survival streak")
-    os.system("clear")
-    print(f"Now playing {modes[selected_mode][0]} mode")
-
-    # gameplay loop here
-    while playing:
-        # gamemodes
-        if selected_mode in [0, 1, 2, 9]: # normal + survive
-            if selected_mode == 1:
-                playing, current_question, current_streak, seeking = gameplay.normal(pairs, shuffled_questions[current_question], current_question_number_enabled=show_current_question_number, streak_current=current_streak, streak_all_time=all_time_streak)
-            elif selected_mode == 9:
-                playing, current_question, current_streak, seeking, lives_left = gameplay.survive(pairs, shuffled_questions[current_question], current_question_number_enabled=show_current_question_number, streak_current=current_streak, streak_all_time=all_time_streak_survival, lives=lives_left)
-            else:
-                playing, current_question, current_streak, seeking = gameplay.normal(pairs, current_question, current_question_number_enabled=show_current_question_number, streak_current=current_streak, streak_all_time=all_time_streak)
-
-        elif selected_mode in [3, 4, 5]: # reverse
-            if selected_mode == 4:
-                playing, current_question, current_streak, seeking = gameplay.reverse(pairs, shuffled_questions[current_question], current_question_number_enabled=show_current_question_number, streak_current=current_streak, streak_all_time=all_time_streak)
-            else:
-                playing, current_question, current_streak, seeking = gameplay.reverse(pairs, current_question, current_question_number_enabled=show_current_question_number, streak_current=current_streak, streak_all_time=all_time_streak)
-
-        elif selected_mode in [6, 7, 8]: # multiple choice
-            if selected_mode == 7:
-                playing, current_question, current_streak, seeking = gameplay.multiple_choice(pairs, shuffled_questions[current_question], current_question_number_enabled=show_current_question_number, streak_current=current_streak, streak_all_time=all_time_streak)
-            else:
-                playing, current_question, current_streak, seeking = gameplay.multiple_choice(pairs, current_question, current_question_number_enabled=show_current_question_number, streak_current=current_streak, streak_all_time=all_time_streak)
-
-        else:
-            raise Exception("gamemode isn't in gameplay modules, I'm honestly surprised this error appeared.")
-
-        # next question for modes
-        if not seeking:
-            if selected_mode in [0, 3, 6]: # plus one
-                if current_question == max_question:
-                    current_question = 1
-                else:
-                    current_question = gameplay.next_question_plus_one(current_question)
-            elif selected_mode in [2, 5, 8]: # minus one
-                if current_question < 0:
-                    current_question = max_question
-                else:
-                    current_question = gameplay.next_question_minus_one(current_question)
-            elif selected_mode in [1, 4, 7, 9]: # random + survive
-                if current_question >= max_question:
-                    current_question = 0
-                    shuffled_questions = core.never_repeat_random_list(min_question, max_question)
-                else:
-                    current_question = gameplay.next_question_plus_one(current_question)
-            else:
-                raise Exception("gamemode isn't in next_questions, I'm honestly surprised this error appeared.")
-
-        elif seeking and selected_mode in [1, 4, 7, 9]: # random + survive seeking
-            try:
-                current_question = shuffled_questions.index(current_question)
-                print(f"Seeking to {current_question}")
-            except ValueError:
-                print(f"{current_question} is an invalid seek number")
-                current_question = shuffled_questions.index(1)
-
-        elif seeking: # seeking normal
-            try:
-                pairs[current_question] # this is for checking if it's a valid index
-                print(f"Seeking to {current_question}")
-            except IndexError:
-                print(f"{current_question} is an invalid seek number")
-                current_question = 1
-
-        if selected_mode == 9 and current_streak > all_time_streak_survival:
-            core.settings_value_manipulator("all time survival streak", "write", current_streak)
-
-        elif current_streak > all_time_streak:
-            core.settings_value_manipulator("all time streak", "write", current_streak)
-
-        all_time_streak = core.settings_value_manipulator("all time streak")
-        all_time_streak_survival = core.settings_value_manipulator("all time survival streak")
-
-        if lives_left == 0:
-            print("Game over!")
-            time.sleep(5)
-            lives_left = core.settings_value_manipulator("lives")
-            streak_current = gameplay.streak_functions(mode="break")
-            playing = False
